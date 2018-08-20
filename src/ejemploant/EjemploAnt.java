ae5add78b140bf6ee3ad2f7ff2db4a58dd8305f4
William Valdovinos <wvaldovinos@sentra.cl>
2018-08-20 02:32:03 -0300

    Se modifica el Saludo
    Se modifica el Saludo

--- src/ejemploant/EjemploAnt.java
+++ src/ejemploant/EjemploAnt.java
@@ -9,7 +9,7 @@ public class EjemploAnt {
         JFrame ventana = new JFrame("Ejemplo");
         ventana.setLocation(100, 100);
         ventana.setSize(500, 100);
-        JLabel etiqueta = new JLabel("Hola mundo");
+        JLabel etiqueta = new JLabel("Hola a Todos");
         ventana.add(etiqueta);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setVisible(true);
